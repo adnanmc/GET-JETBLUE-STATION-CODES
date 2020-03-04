97eaec869bfa01e746b5d8736ecbb4f03853e245
adnanmc <adnanmcj@gmail.com>
2020-03-04 16:00:04 -0500

    Added baseline code

---
+++ index.js
@@ -0,0 +1,41 @@
+const siteUrl = "https://www.jetblue.com/destinations";
+const cheerio = require("cheerio");
+const axios = require("axios");
+const fs = require("fs");
+const fetchData = async () => {
+  const result = await axios.get(siteUrl);
+  return cheerio.load(result.data);
+};
+
+const getResults = async () => {
+  const $ = await fetchData();
+  const data = {};
+  const checkForParentheses = RegExp(/\ \(/);
+  $("a").each(function(index, element) {
+    let innerText = $(this)
+      .text()
+      .trimLeft()
+      .trimRight();
+    if (checkForParentheses.test(innerText)) {
+      let city = innerText.slice(0, innerText.indexOf(" ("));
+      let airportCode = innerText.substring(
+        innerText.indexOf("(") + 1,
+        innerText.indexOf(")")
+      );
+      data[airportCode] = city;
+    }
+  });
+  let finalData = JSON.stringify(data);
+//   console.log(finalData);
+  let fileName = "stationCode.json"
+  fs.writeFile(fileName, finalData, "utf8", err => {
+    if (err) {
+      console.log(err);
+    } else {
+        console.log(`File: ${fileName} has been saved.`);
+        
+    }
+  });
+};
+
+getResults();
